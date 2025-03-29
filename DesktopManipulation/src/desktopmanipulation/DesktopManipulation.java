ac27c617225dca57a847ba579040ad8c551aaa14
Henrique Chaves Duarte <ht3036995@hto.ifsp.edu.br>
2025-03-28 21:08:24 -0300

    cod documentado

--- DesktopManipulation/src/desktopmanipulation/DesktopManipulation.java
+++ DesktopManipulation/src/desktopmanipulation/DesktopManipulation.java
@@ -22,28 +22,38 @@ public class DesktopManipulation {
         }
     }
     
-    private static void runSearch (URI url) {
-        try {
+    private static void runSearch (URI url) { 
+        
+        if(Desktop.isDesktopSupported()){ //conferir se o sistema suporta
+               
             Desktop desk = null;
-            desk = desk.getDesktop();
-            desk.browse(url);
+            desk = desk.getDesktop();  //cria meio que a interface para que o java mexa com o sistema operacional
 
-        } catch (Exception e) {
-            System.out.println ("Deu errado: " + e + "\n");
-            System.out.println ("URI attempt: " + url + "\n");
-            System.out.println("Aperte algo para continuar...");
-            new java.util.Scanner(System.in).nextLine();
+            try {
+
+                desk.browse(url); //inicia o navegador padrão na url
+
+            } catch (Exception e) {
+                System.out.println ("Deu errado: " + e + "\n");
+                System.out.println ("URI attempt: " + url + "\n");
+                System.out.println("Aperte algo para continuar...");
+                new java.util.Scanner(System.in).nextLine();
+            }
+        }else{
+            System.out.println("Desktop não é suportado neste sistema.");
         }
     }
     
     private static void typeText (String text) {
         try {
             Robot robot = new Robot();
-            robot.delay(2000);
+            robot.delay(2000); // depois de instanciar, delay de 2seg
             String sample = "Em português, me dê descrição completa e bem estruturada sobre a seguinte palavra: \"" + text + "\"" + ". Se possível, me passe fontes para sua descrição.";
-            StringSelection stringSelection = new StringSelection(sample);
-            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-            clipboard.setContents(stringSelection, stringSelection);
+            StringSelection stringSelection = new StringSelection(sample); //cria um objeto que contém o texto para transferir para a área de transferência
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard(); //retorna a área de transferencia do sistema
+            clipboard.setContents(stringSelection, stringSelection); //coloca os dados da string no clipboard
+            
+            //robo apertando os botões
             robot.keyPress(KeyEvent.VK_ALT);
             robot.delay(300);
             robot.keyPress(KeyEvent.VK_SPACE);
@@ -62,6 +72,7 @@ public class DesktopManipulation {
         }
     }
     
+    // pega a palavra que o usuario digita
     private static String getUserInput () {
         System.out.print ("Defina uma palavra: ");
         Scanner scan = new Scanner(System.in);
@@ -69,13 +80,13 @@ public class DesktopManipulation {
     }
     
     public static void main(String[] args) {
-        URI url = null;
-        String userInput = getUserInput();
-        url = url.create("https://www.blackbox.ai/");
+        URI url = null;  // URI = tipo url
+        String userInput = getUserInput(); 
+        url = url.create("https://www.blackbox.ai/"); 
         runSearch(url);
         typeText(userInput);
         Robot roboto = null;
-        roboto = instantiateRobot(roboto);
+        roboto = instantiateRobot(roboto); //instancia o roboto
         roboto.keyPress(KeyEvent.VK_ENTER);
         roboto.keyRelease(KeyEvent.VK_ENTER);
     }       
