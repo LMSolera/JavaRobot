f5acf10caa69925be0e77c074d32e222b0b2f056
Henrique Chaves Duarte <ht3036995@hto.ifsp.edu.br>
2025-05-30 21:45:14 -0300

    modificações controler upload

--- src/main/java/com/lpn3/licitamatch/view/UploadController.java
+++ src/main/java/com/lpn3/licitamatch/view/UploadController.java
@@ -1,26 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.lpn3.licitamatch.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author hduar
- */
 public class UploadController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    //Initializes the controller class.
+    
+    @FXML private Button botaoProposta;
+    @FXML private Button botaoLicitacao;
+    @FXML private Button botaoComparar;
+    @FXML private Label labelProposta;
+    @FXML private Label labelArquivo;
+    @FXML private Label labelResultado;
+
+    private File file1;
+    private File file2;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }    
+    
+    @FXML
+    private void selecionarArquivoProposta(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Selecionar Arquivo 1");
+        File selectedFile = fileChooser.showOpenDialog(getStage());
+
+        if (selectedFile != null) {
+            file1 = selectedFile;
+            labelArquivo1.setText(selectedFile.getName());
+            
+            File destinationFolder = new File(./sentFiles)
+        
+        }
+    }
     
 }
