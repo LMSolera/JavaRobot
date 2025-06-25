12ba0512974e508a872765e32bed71d314323cd4
Henrique <hcduarte200@gmail.com>
2025-06-24 22:24:31 -0300

    bug no css, irei dar uma olhada melhor depois.

--- src/main/java/com/lpn3/licitamatch/application/Main.java
+++ src/main/java/com/lpn3/licitamatch/application/Main.java
@@ -5,32 +5,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class Main extends Application {
-    
+
     @Override 
     public void start(Stage primaryStage) {
         try {
-            // 1. Carrega o arquivo FXML (a View)
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Upload.fxml")); 
-
-            // 2. Cria a Cena
+            // Carrega a tela de Login como inicial
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
             Scene scene = new Scene(root);
 
-            // 3. Configura o Palco (a janela principal)
-            primaryStage.setTitle("Tela de upload");
+            primaryStage.setTitle("Login - LicitaMatch");
             primaryStage.setScene(scene);
-            
-            // 4. Mostra a janela
             primaryStage.show();
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
     
-    public static void main (String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 }
