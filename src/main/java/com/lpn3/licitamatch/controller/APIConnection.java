e0e6e55de9f338bd569e36c86b95c14d3e38b882
LMSolera <lumi.solera@gmail.com>
2025-06-26 14:40:30 -0300

    Aceita os arquivos escolhidos pelo usuário na tela gráfica, e envia eles na requisição para o OpenRouter

--- src/main/java/com/lpn3/licitamatch/controller/APIConnection.java
+++ src/main/java/com/lpn3/licitamatch/controller/APIConnection.java
@@ -1,6 +1,8 @@
 package com.lpn3.licitamatch.controller;
 
 import com.google.gson.Gson;
+
+import java.io.File;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,24 +10,69 @@ import java.net.http.HttpResponse;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Map;
-
-
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 
 public class APIConnection {
-    public static void main(String[] args) {
+    
+    static File licitacao = null;
+    static File proposta = null;
+    static String dadosProposta;
+    static String dadosLicitacao;
+    
+    public static void compararDocumentos(File arquivoProposta, File arquivoLicitacao) {
         String openRouterApiKey = "Chave";
         
         String openRouterApiUrl = "https://openrouter.ai/api/v1/chat/completions";
-
+        
+        proposta = arquivoProposta;
+        licitacao = arquivoLicitacao;
+        
+        try {
+            PDDocument documentoProposta;
+            documentoProposta = PDDocument.load(proposta);
+            PDFTextStripper stripperDeTexto = new PDFTextStripper();
+            dadosProposta = stripperDeTexto.getText(documentoProposta);
+            documentoProposta.close();
+        } catch (Exception e) {
+            System.out.println("Excecao ao tentar ler conteúdo da Proposta: " + e);
+            e.printStackTrace();
+        }
+        
+        try {
+            PDDocument documentoLicitacao;
+            documentoLicitacao = PDDocument.load(licitacao);
+            PDFTextStripper stripperDeTexto = new PDFTextStripper();
+            dadosLicitacao = stripperDeTexto.getText(documentoLicitacao);
+            documentoLicitacao.close();
+        } catch (Exception e) {
+            System.out.println("Excecao ao tentar ler conteúdo da Licitacao: " + e);
+            e.printStackTrace();
+        }
+        
         String jsonInputString = """
         {
           "model": "deepseek/deepseek-r1-0528:free",
           "messages": [
-            {"role": "system", "content": "Você é um profesor"},
-            {"role": "user", "content": "Em português, explique o teorema de pitágoras sem adição de "*" na resposta."}
+            {"role": "system", "content": "Você tem como objetivo comparar documentos de maneira crítica e analítica"},
+            {"role": "user", "content": 
+                                 "Com base nessa licitação:
+                                 """ + dadosLicitacao + """                             
+                                 Realize a análise da seguinte proposta:
+                                 """ + dadosProposta + """                       
+                                 Ao final da análise, você deve avaliar a proposta trazendo as seguintes explicações:
+                                    Características Similares:
+                                    Características Diferentes:
+                                    Nota de análise de 0 - 10:
+                                 A nota deve considerar tais características:
+                                    0 - 3 : Com base nas diferenças de custo.
+                                    0 - 3 : Com base nas diferenças de produto esperado, e produto ofertado.
+                                    0 - 2 : Com base na data de entrega esperada e data de entrega ofertada.
+                                    0 - 2 : Com base na estrutura da proposta.                                          
+                                 "}
           ],
-          "max_tokens": 1000,
+          "max_tokens": 5000,
           "temperature": 0.7
         }
         """;
@@ -77,7 +124,7 @@ public class APIConnection {
                 }
                 
             } else {
-                System.err.println("Erro na requisição. Detalhes acima."); 
+                System.err.println("Erro na requisição."); 
             }
 
         } catch (IOException | InterruptedException e) {
